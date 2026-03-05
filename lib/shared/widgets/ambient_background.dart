89420a86ac3e2a691c433a7aae8f6ad05350335d
blancodagoat <69021803+blancodagoat@users.noreply.github.com>
2026-03-05 02:08:20 +0100

    Add performance TODOs and initial RepaintBoundary optimization

--- lib/shared/widgets/ambient_background.dart
+++ lib/shared/widgets/ambient_background.dart
@@ -5,6 +5,13 @@ import '../../core/constants/app_colors.dart';
 class AmbientBackground extends StatelessWidget {
   const AmbientBackground({super.key});
 
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import '../../core/constants/app_colors.dart';
+
+class AmbientBackground extends StatelessWidget {
+  const AmbientBackground({super.key});
+
   @override
   Widget build(BuildContext context) {
     return RepaintBoundary(
@@ -17,7 +24,7 @@ class AmbientBackground extends StatelessWidget {
               child: Container(
                 width: 380,
                 height: 380,
-                decoration: BoxDecoration(
+                decoration: const BoxDecoration(
                   shape: BoxShape.circle,
                   gradient: RadialGradient(
                     colors: [
@@ -29,17 +36,31 @@ class AmbientBackground extends StatelessWidget {
               )
                   .animate(onPlay: (c) => c.repeat(reverse: true))
                   .moveY(begin: 0, end: 20, duration: 8.seconds, curve: Curves.easeInOut),
+              ),
             ),
-            const Positioned(
+            Positioned(
               bottom: 100,
               right: -40,
-              child: _StaticGlow(),
+              child: Container(
+                width: 220,
+                height: 220,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: RadialGradient(
+                    colors: [
+                      AppColors.positive.withOpacity(0.07),
+                      Colors.transparent,
+                    ],
+                  ),
+                ),
+              ),
             ),
           ],
         ),
       ),
     );
   }
+}
 }
 
 class _StaticGlow extends StatelessWidget {
