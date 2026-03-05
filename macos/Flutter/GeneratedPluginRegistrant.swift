a4435beb37da1f21e33b2c01f42ff7a8e3c59ecd
blancodagoat <69021803+blancodagoat@users.noreply.github.com>
2026-03-05 01:44:48 +0100

    Add iOS finance app utilities and upgrade linting
    Added:
    - local_auth: Face ID / Touch ID biometric authentication
    - share_plus: iOS share sheet for exporting transactions
    - shared_preferences: User settings storage
    - flutter_secure_storage: Secure token storage (Keychain)
    - animations: Material motion system
    - very_good_analysis: Strict linting (replaced flutter_lints)
    
    Updated analysis_options.yaml to use very_good_analysis

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,18 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_secure_storage_macos
 import isar_flutter_libs
+import local_auth_darwin
 import package_info_plus
+import share_plus
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
+  LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
